88e1cc276ff45d71c7361e23a19cecaf9b38ad74
wadvance <barj23535@gmail.com>
2026-06-14 11:38:51 -0500

    deploy: eliminar Panama y Avances

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "sop-optica-v4";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("/"))),
+  );
+});
